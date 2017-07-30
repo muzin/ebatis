dd0890109c8ea89220f7c733435ccae6397adc51
muzin <215667266@qq.com>
2017-07-30 19:56:19 +0000

    add components for ebatis.

---
+++ EbatisConfig.js
@@ -0,0 +1,27 @@
+
+var config = {
+
+    datasource : [
+        {
+            host : '',
+            port : '',
+            username : '',
+            password : ''
+        },
+        {
+
+        }
+    ],
+
+    // or
+
+    datasource : {
+        host : '',
+        port : '',
+        username : '',
+        password : ''
+    }
+
+}
+
+exports = module.exports = config;
